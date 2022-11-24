c9b30b6e8e014305e5454c15528b248681ad02ef
Nico Audy <nicoaudy1994@gmail.com>
2022-11-24 11:52:21 +0700

    remove testing file

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,8 +2,6 @@
 
 namespace App\Providers;
 
-use App\Events\PostCreated;
-use App\Listeners\SendPostCreatedNotifications;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
@@ -17,10 +15,6 @@ class EventServiceProvider extends ServiceProvider
      * @var array<class-string, array<int, class-string>>
      */
     protected $listen = [
-
-        PostCreated::class => [
-            SendPostCreatedNotifications::class,
-        ],
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
