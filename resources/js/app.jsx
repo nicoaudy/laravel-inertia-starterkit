4d500075f08363a12be8ad3d87733e2804b7ba27
Nico Audy <nicoaudy1994@gmail.com>
2022-11-30 10:11:25 +0700

    change arrowparens to always

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -12,11 +12,11 @@ const appName =
 
 createInertiaApp({
   title: title => `${title} - ${appName}`,
-    resolve: name =>
-      resolvePageComponent(
-        `./Pages/${name}.jsx`,
-        import.meta.glob('./Pages/**/*.jsx')
-      ),
+  resolve: name =>
+    resolvePageComponent(
+      `./Pages/${name}.jsx`,
+      import.meta.glob('./Pages/**/*.jsx')
+    ),
   setup({ el, App, props }) {
     const root = createRoot(el);
     root.render(<App {...props} />);
