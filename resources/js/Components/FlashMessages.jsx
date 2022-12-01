b786d93dbf8c7ae490708168befbd9a12f1e6a0e
NicoAudy <nicoaudy1994@gmail.com>
2022-12-01 23:16:13 +0700

    prettier

--- resources/js/Components/FlashMessages.jsx
+++ resources/js/Components/FlashMessages.jsx
@@ -1,5 +1,5 @@
-import { useState, useEffect } from 'react'
-import { usePage } from '@inertiajs/inertia-react'
+import { useState, useEffect } from 'react';
+import { usePage } from '@inertiajs/inertia-react';
 
 const FlashMessages = () => {
   const [visible, setVisible] = useState(true);
@@ -9,8 +9,8 @@ const FlashMessages = () => {
     setVisible(true);
 
     setTimeout(() => {
-      setVisible(false)
-    }, 5000)
+      setVisible(false);
+    }, 5000);
   }, [flash]);
 
   return (
@@ -18,17 +18,39 @@ const FlashMessages = () => {
       {flash.success && visible && (
         <div className="flex items-center justify-between max-w-xs transition duration-300 p-2 bg-white border rounded-md shadow-lg space-x-4">
           <div className="flex items-center">
-            <svg xmlns="http://www.w3.org/2000/svg" className="w-8 h-8 text-green-500" viewBox="0 0 20 20" fill="currentColor">
-              <path fillRule="evenodd"
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="w-8 h-8 text-green-500"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fillRule="evenodd"
                 d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                clipRule="evenodd" />
+                clipRule="evenodd"
+              />
             </svg>
-            <p className="ml-3 mr-2 text-xs font-medium text-gray-600">{flash.success}</p>
+            <p className="ml-3 mr-2 text-xs font-medium text-gray-600">
+              {flash.success}
+            </p>
           </div>
-          <span className="inline-flex items-center cursor-pointer" onClick={() => setVisible(false)}>
-            <svg xmlns="http://www.w3.org/2000/svg" className="w-4 h-4 text-gray-400" fill="none" viewBox="0 0 24 24"
-              stroke="currentColor">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
+          <span
+            className="inline-flex items-center cursor-pointer"
+            onClick={() => setVisible(false)}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="w-4 h-4 text-gray-400"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M6 18L18 6M6 6l12 12"
+              />
             </svg>
           </span>
         </div>
@@ -37,17 +59,39 @@ const FlashMessages = () => {
       {flash.info && visible && (
         <div className="flex items-center justify-between max-w-xs p-2 bg-white border rounded-md shadow-lg space-x-4">
           <div className="flex items-center">
-            <svg xmlns="http://www.w3.org/2000/svg" className="w-8 h-8 text-indigo-600" viewBox="0 0 20 20" fill="currentColor">
-              <path fillRule="evenodd"
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="w-8 h-8 text-indigo-600"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fillRule="evenodd"
                 d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
-                clipRule="evenodd" />
+                clipRule="evenodd"
+              />
             </svg>
-            <p className="ml-3 mr-2 text-xs font-medium text-gray-600">{flash.info}</p>
+            <p className="ml-3 mr-2 text-xs font-medium text-gray-600">
+              {flash.info}
+            </p>
           </div>
-          <span className="inline-flex items-center cursor-pointer" onClick={() => setVisible(false)}>
-            <svg xmlns="http://www.w3.org/2000/svg" className="w-4 h-4 text-gray-400" fill="none" viewBox="0 0 24 24"
-              stroke="currentColor">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
+          <span
+            className="inline-flex items-center cursor-pointer"
+            onClick={() => setVisible(false)}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="w-4 h-4 text-gray-400"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M6 18L18 6M6 6l12 12"
+              />
             </svg>
           </span>
         </div>
@@ -56,17 +100,39 @@ const FlashMessages = () => {
       {flash.warning && visible && (
         <div className="flex items-center justify-between max-w-xs p-2 bg-white border rounded-md shadow-lg space-x-4">
           <div className="flex items-center">
-            <svg xmlns="http://www.w3.org/2000/svg" className="w-8 h-8 text-yellow-600" viewBox="0 0 20 20" fill="currentColor">
-              <path fillRule="evenodd"
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="w-8 h-8 text-yellow-600"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fillRule="evenodd"
                 d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z"
-                clipRule="evenodd" />
+                clipRule="evenodd"
+              />
             </svg>
-            <p className="ml-3 mr-2 text-xs font-medium text-gray-600">{flash.warning}</p>
+            <p className="ml-3 mr-2 text-xs font-medium text-gray-600">
+              {flash.warning}
+            </p>
           </div>
-          <span className="inline-flex items-center cursor-pointer" onClick={() => setVisible(false)}>
-            <svg xmlns="http://www.w3.org/2000/svg" className="w-4 h-4 text-gray-400" fill="none" viewBox="0 0 24 24"
-              stroke="currentColor">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
+          <span
+            className="inline-flex items-center cursor-pointer"
+            onClick={() => setVisible(false)}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="w-4 h-4 text-gray-400"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M6 18L18 6M6 6l12 12"
+              />
             </svg>
           </span>
         </div>
@@ -75,23 +141,45 @@ const FlashMessages = () => {
       {flash.error && visible && (
         <div className="flex items-center justify-between max-w-xs p-2 bg-white border rounded-md shadow-lg space-x-4">
           <div className="flex items-center">
-            <svg xmlns="http://www.w3.org/2000/svg" className="w-8 h-8 text-red-600" viewBox="0 0 20 20" fill="currentColor">
-              <path fillRule="evenodd"
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="w-8 h-8 text-red-600"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fillRule="evenodd"
                 d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7 4a1 1 0 11-2 0 1 1 0 012 0zm-1-9a1 1 0 00-1 1v4a1 1 0 102 0V6a1 1 0 00-1-1z"
-                clipRule="evenodd" />
+                clipRule="evenodd"
+              />
             </svg>
-            <p className="ml-3 mr-2 text-xs font-medium text-gray-600">{flash.error}</p>
+            <p className="ml-3 mr-2 text-xs font-medium text-gray-600">
+              {flash.error}
+            </p>
           </div>
-          <span className="inline-flex items-center cursor-pointer" onClick={() => setVisible(false)}>
-            <svg xmlns="http://www.w3.org/2000/svg" className="w-4 h-4 text-gray-400" fill="none" viewBox="0 0 24 24"
-              stroke="currentColor">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
+          <span
+            className="inline-flex items-center cursor-pointer"
+            onClick={() => setVisible(false)}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="w-4 h-4 text-gray-400"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M6 18L18 6M6 6l12 12"
+              />
             </svg>
           </span>
         </div>
       )}
     </div>
-  )
-}
+  );
+};
 
-export default FlashMessages
+export default FlashMessages;
