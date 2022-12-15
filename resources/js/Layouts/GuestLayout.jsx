cffb69cfcd04bfe08b74416148792458d8f665c1
Nico Audy <nicoaudy1994@gmail.com>
2022-12-15 11:52:56 +0700

    remove logo

--- resources/js/Layouts/GuestLayout.jsx
+++ resources/js/Layouts/GuestLayout.jsx
@@ -1,12 +1,12 @@
-import Logo from '@/Components/Logo';
 import { Link } from '@inertiajs/react';
+import { IconBrandLaravel } from '@tabler/icons';
 
 export default function Guest({ children }) {
   return (
     <div className="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
       <div>
         <Link href="/">
-          <Logo className="w-20 h-20 fill-current text-gray-500" height={50} />
+          <IconBrandLaravel size={40} color="gray" />
         </Link>
       </div>
 
