20d5c76e4e4a66ed0520039a33025db463ea4cbb
Nico Audy <nicoaudy1994@gmail.com>
2023-05-16 15:56:44 +0700

    ts migration

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -13,7 +13,7 @@
     <!-- Scripts -->
     @routes
     @viteReactRefresh
-    @vite('resources/js/app.jsx')
+    @vite(['resources/js/app.tsx', "resources/js/Pages/{$page['component']}.tsx"])
     @inertiaHead
 </head>
 
